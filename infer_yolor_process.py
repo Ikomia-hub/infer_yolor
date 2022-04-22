a1d39b62ede8841e3d621c9cbb152a7915b4806a
Ludo <ludovic.barusseau@gmail.com>
2022-04-22 12:39:22 +0200

    API change for getProgressSteps() function

--- infer_yolor_process.py
+++ infer_yolor_process.py
@@ -98,7 +98,7 @@ class YoloRProcess(dataprocess.C2dImageTask):
         else:
             self.setParam(copy.deepcopy(param))
 
-    def getProgressSteps(self, eltCount=1):
+    def getProgressSteps(self):
         # Function returning the number of progress steps for this process
         # This is handled by the main progress bar of Ikomia application
         return 1
